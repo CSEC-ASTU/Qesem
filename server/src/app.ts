8b9aef2f1202e8863e05518182586bf7dd5a1287
mroxygen2024 <fuad.sano@astu.edu.et>
2026-01-22 21:10:14 +0300

    feat: model and  upload feature added

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,13 @@ import cors from 'cors'
 import uploadRouter from './routes/upload.js'
 import questionRouter from './routes/question.js'
 import quizRouter from './routes/quiz.js'
+import mongoose from 'mongoose'
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/qesem'
+
+mongoose.connect(MONGO_URI).catch((err) => {
+	console.error('Mongo connection error', err)
+})
 
 const app = express()
 
